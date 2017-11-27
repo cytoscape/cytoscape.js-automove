3edd55aefda6e29e145e92c404dea6ec7aa02231
Max Franz <maxkfranz@gmail.com>
2017-11-27 12:36:07 -0500

    Updates

---
+++ README.md
@@ -0,0 +1,173 @@
+cytoscape-automove
+================================================================================
+
+
+## Description
+
+An extension for Cytoscape.js that automatically updates node positions based on specified rules ([demo](https://cytoscape.github.io/cytoscape.js-automove))
+
+Common usecases:
+
+ * Making one node move in step with another node
+ * Constraining a node within a boundary
+ * Using a node to represent an n-ary interaction
+
+
+## Dependencies
+
+ * Cytoscape.js ^3.2.0
+
+
+## Usage instructions
+
+Download the library:
+ * via npm: `npm install cytoscape-automove`,
+ * via bower: `bower install cytoscape-automove`, or
+ * via direct download in the repository (probably from a tag).
+
+Import the library as appropriate for your project:
+
+ES import:
+
+```js
+import cytoscape from 'cytoscape';
+import automove from 'cytoscape-automove';
+
+cytoscape.use( automove );
+```
+
+CommonJS require:
+
+```js
+let cytoscape = require('cytoscape');
+let automove = require('cytoscape-automove');
+
+cytoscape.use( automove ); // register extension
+```
+
+AMD:
+
+```js
+require(['cytoscape', 'cytoscape-automove'], function( cytoscape, automove ){
+  automove( cytoscape ); // register extension
+});
+```
+
+Plain HTML/JS has the extension registered for you automatically, because no `require()` is needed.
+
+
+## API
+
+Each time `cy.automove()` is called, the specified rules are added to the core instance:
+
+```js
+let defaults = {
+  // specify nodes that should be automoved with one of
+  // - a function that returns true for matching nodes
+  // - a selector that matches the nodes
+  // - a collection of nodes (very good for performance)
+  nodesMatching: function( node ){ return false; },
+
+  // specify how a node's position should be updated with one of
+  // - function( node ){ return { x: 1, y: 2 }; } => put the node where the function returns
+  // - { x1, y1, x2, y2 } => constrain the node position within the bounding box (in model co-ordinates)
+  // - 'mean' => put the node in the average position of its neighbourhood
+  // - 'viewport' => keeps the node body within the viewport
+  // - 'drag' => matching nodes are effectively dragged along
+  reposition: 'mean',
+
+  // specify when the repositioning should occur by specifying a function that
+  // calls update() when reposition updates should occur
+  // - function( update ){ /* ... */ update(); } => a manual function for updating
+  // - 'matching' => automatically update on position events for nodesMatching
+  // - set efficiently and automatically for
+  //   - reposition: 'mean'
+  //   - reposition: { x1, y1, x2, y2 }
+  //   - reposition: 'viewport'
+  //   - reposition: 'drag'
+  // - default/undefined => on a position event for any node (not as efficient...)
+  when: undefined,
+
+
+
+  //
+  // customisation options for non-function `reposition` values
+  //
+
+  // `reposition: 'mean'`
+
+    // specify nodes that should be ignored in the mean calculation
+    // - a function that returns true for nodes to be ignored
+    // - a selector that matches the nodes to be ignored
+    // - a collection of nodes to be ignored (very good for performance)
+    meanIgnores: function( node ){ return false; },
+
+    // specify whether moving a particular `nodesMatching` node causes repositioning
+    // - true : the mid node can't be independently moved/dragged
+    // - false : the mid node can be independently moved/dragged (useful if you want the mid node to use `reposition: 'drag' in another rule with its neighbourhood`)
+    meanOnSelfPosition: function( node ){ return true; },
+
+  // `reposition: 'drag'`
+
+    // specify nodes that when dragged cause the matched nodes to move along (i.e. the master nodes)
+    // - a function that returns true for nodes to be listened to for drag events
+    // - a selector that matches the nodes to be listened to for drag events
+    // - a collection of nodes to be listened to for drag events (very good for performance)
+    dragWith: function( node ){ return false; }
+};
+
+let options = defaults;
+
+let rule = cy.automove( options );
+```
+
+A rule has a number of functions available:
+
+```js
+rule.apply(); // manually apply a rule
+
+rule.enabled(); // get whether rule is enabled
+
+rule.toggle(); // toggle whether the rule is enabled
+
+rule.disable(); // temporarily disable the rule
+
+rule.enable(); // re-enable the rule
+
+rule.destroy(); // remove and clean up just this rule
+```
+
+You can also remove all the rules you previously specified:
+
+```js
+cy.automove('destroy');
+```
+
+## Events
+
+- `automove` : Emitted on a node when its position is changed by a rule
+  - `node.on('automove', function( event, rule ){})`
+
+
+## Build targets
+
+* `npm run test` : Run Mocha tests in `./test`
+* `npm run build` : Build `./src/**` into `cytoscape-automove.js`
+* `npm run watch` : Automatically build on changes with live reloading (N.b. you must already have an HTTP server running)
+* `npm run dev` : Automatically build on changes with live reloading with webpack dev server
+* `npm run lint` : Run eslint on the source
+
+N.b. all builds use babel, so modern ES features can be used in the `src`.
+
+
+## Publishing instructions
+
+This project is set up to automatically be published to npm and bower.  To publish:
+
+1. Build the extension : `npm run build`
+1. Commit the build : `git commit -am "Build for release"`
+1. Bump the version number and tag: `npm version major|minor|patch`
+1. Push to origin: `git push && git push --tags`
+1. Publish to npm: `npm publish .`
+1. If publishing to bower for the first time, you'll need to run `bower register cytoscape-automove https://github.com/cytoscape/cytoscape.js-automove.git`
+1. [Make a new release](https://github.com/cytoscape/cytoscape.js-automove/releases/new) for Zenodo.
---
+++ src/automove.js
@@ -0,0 +1,417 @@
+let defaults = require('./defaults');
+
+let typeofStr = typeof '';
+let typeofObj = typeof {};
+let typeofFn = typeof function(){};
+
+let isObject = function( x ){ return typeof x === typeofObj; };
+let isString = function( x ){ return typeof x === typeofStr; };
+let isFunction = function( x ){ return typeof x === typeofFn; };
+let isCollection = function( x ){ return isObject( x ) && isFunction( x.collection ); };
+
+// Object.assign() polyfill
+let assign = require('./assign');
+
+let eleExists = function( ele ){
+  return ele != null && !ele.removed();
+};
+
+let elesHasEle = function( eles, ele ){
+  if( eles.has != undefined ){ // 3.x
+    elesHasEle = function( eles, ele ){ return eles.has( ele ); };
+  } else { // 2.x
+    elesHasEle = function( eles, ele ){ return eles.intersection( ele ).length > 0; };
+  }
+
+  return elesHasEle( eles, ele );
+};
+
+let getEleMatchesSpecFn = function( spec ){
+  if( isString( spec ) ){
+    return function( ele ){
+      return ele.is( spec );
+    };
+  } else if( isFunction( spec ) ){
+    return spec;
+  } else if( isCollection( spec ) ){
+    return function( ele ){
+      return elesHasEle( spec, ele );
+    };
+  } else {
+    throw new Error('Can not create match function for spec', spec);
+  }
+};
+
+let bindings = [];
+
+let bind = function( cy, events, selector, fn ){
+  let b = { cy: cy, events: events, selector: selector || 'node', fn: fn };
+
+  bindings.push( b );
+
+  cy.on( b.events, b.selector, b.fn );
+
+  return b;
+};
+
+let bindOnRule = function( rule, cy, events, selector, fn ){
+  let b = bind( cy, events, selector, fn );
+  let bindings = rule.bindings = rule.bindings || [];
+
+  bindings.push( b );
+};
+
+let unbindAllOnRule = function( rule ){
+  let unbind = function( b ){ b.cy.off( b.events, b.selector, b.fn ); };
+
+  rule.bindings.forEach( unbind );
+
+  rule.bindings = [];
+};
+
+let getRepositioner = function( rule, cy ){
+  let r = rule.reposition;
+
+  if( r === 'mean' ){
+    return meanNeighborhoodPosition( getEleMatchesSpecFn( rule.meanIgnores ) );
+  } else if( r === 'viewport' ){
+    return viewportPosition( cy );
+  } else if( r === 'drag' ){
+    return dragAlong( rule );
+  } else if( isObject( r ) ){
+    return boxPosition( r );
+  } else {
+    return r;
+  }
+};
+
+let dragAlong = function( rule ){
+  return function( node ){
+    let pos = node.position();
+    let delta = rule.delta;
+
+    if( rule.delta != null && !node.same( rule.grabbedNode ) && !node.grabbed() ){
+      return {
+        x: pos.x + delta.x,
+        y: pos.y + delta.y
+      };
+    }
+  };
+};
+
+let meanNeighborhoodPosition = function( ignore ){
+  return function( node ){
+    let nhood = node.neighborhood();
+    let avgPos = { x: 0, y: 0 };
+    let nhoodSize = 0;
+
+    for( let i = 0; i < nhood.length; i++ ){
+      let nhoodEle = nhood[i];
+
+      if( nhoodEle.isNode() && !ignore( nhoodEle ) ){
+        let pos = nhoodEle.position();
+
+        avgPos.x += pos.x;
+        avgPos.y += pos.y;
+
+        nhoodSize++;
+      }
+    }
+
+    // the position should remain unchanged if we would stack the nodes on top of each other
+    if( nhoodSize < 2 ){
+      return undefined;
+    }
+
+    avgPos.x /= nhoodSize;
+    avgPos.y /= nhoodSize;
+
+    return avgPos;
+  };
+};
+
+let constrain = function( val, min, max ){
+  return val < min ? min : ( val > max ? max : val );
+};
+
+let constrainInBox = function( node, bb ){
+  let pos = node.position();
+
+  return {
+    x: constrain( pos.x, bb.x1, bb.x2 ),
+    y: constrain( pos.y, bb.y1, bb.y2 )
+  };
+};
+
+let boxPosition = function( bb ){
+  return function( node ){
+    return constrainInBox( node, bb );
+  };
+};
+
+let viewportPosition = function( cy ){
+  return function( node ){
+    let extent = cy.extent();
+    let w = node.outerWidth();
+    let h = node.outerHeight();
+    let bb = {
+      x1: extent.x1 + w/2,
+      x2: extent.x2 - w/2,
+      y1: extent.y1 + h/2,
+      y2: extent.y2 - h/2
+    };
+
+    return constrainInBox( node, bb );
+  };
+};
+
+let meanListener = function( rule ){
+  return function( update, cy ){
+    let matches = function( ele ){
+      // must meet ele set and be connected to more than (1 edge + 1 node)
+      return rule.matches( ele ) && ele.neighborhood().length > 2 && !ele.grabbed();
+    };
+
+    bindOnRule( rule, cy, 'position', 'node', function(){
+      let movedNode = this;
+
+      if(
+        movedNode.neighborhood().some( matches ) ||
+        ( rule.meanOnSelfPosition( movedNode ) && matches( movedNode ) )
+      ){
+        update( cy, [ rule ] );
+      }
+    });
+
+    bindOnRule( rule, cy, 'add remove', 'edge', function(){
+      let edge = this;
+      let src = cy.getElementById( edge.data('source') );
+      let tgt = cy.getElementById( edge.data('target') );
+
+      if( [ src, tgt ].some( matches ) ){
+        update( cy, [ rule ] );
+      }
+    });
+  };
+};
+
+let dragListener = function( rule ){
+  return function( update, cy ){
+    bindOnRule( rule, cy, 'grab', 'node', function(){
+      let node = this;
+
+      if( rule.dragWithMatches( node ) ){
+        let p = node.position();
+
+        rule.grabbedNode = node;
+        rule.p1 = { x: p.x, y: p.y };
+        rule.delta = { x: 0, y: 0 };
+      }
+    });
+
+    bindOnRule( rule, cy, 'drag', 'node', function(){
+      let node = this;
+
+      if( node.same( rule.grabbedNode ) ){
+        let d = rule.delta;
+        let p1 = rule.p1;
+        let p = node.position();
+        let p2 = { x: p.x, y: p.y };
+
+        d.x = p2.x - p1.x;
+        d.y = p2.y - p1.y;
+
+        rule.p1 = p2;
+
+        update( cy, [ rule ] );
+      }
+    });
+
+    bindOnRule( rule, cy, 'free', 'node', function(){
+      rule.grabbedNode = null;
+      rule.delta = null;
+      rule.p1 = null;
+    });
+  };
+};
+
+let matchingNodesListener = function( rule ){
+  return function( update, cy ){
+    bindOnRule( rule, cy, 'position', 'node', function(){
+      let movedNode = this;
+
+      if( rule.matches( movedNode ) ){
+        update( cy, [ rule ] );
+      }
+    });
+  };
+};
+
+let getListener = function( cy, rule ){
+  if( rule.reposition === 'mean' ){
+    return meanListener( rule );
+  } else if( rule.reposition === 'drag' ){
+    return dragListener( rule );
+  } else if(
+    isObject( rule.reposition )
+    || rule.when === 'matching'
+    || rule.reposition === 'viewport'
+  ){
+    return matchingNodesListener( rule );
+  } else {
+    return rule.when;
+  }
+};
+
+let addRule = function( cy, scratch, options ){
+  let rule = assign( {}, defaults, options );
+
+  rule.getNewPos = getRepositioner( rule, cy );
+  rule.listener = getListener( cy, rule );
+
+  let nodesAreCollection = isCollection( rule.nodesMatching );
+
+  if( nodesAreCollection ){
+    rule.nodes = rule.nodesMatching;
+
+    rule.matches = function( ele ){ return eleExists( ele ) && elesHasEle( rule.nodes, ele ); };
+  } else {
+    let matches = getEleMatchesSpecFn( rule.nodesMatching );
+
+    rule.matches = function( ele ){ return eleExists( ele ) && matches( ele ); };
+  }
+
+  if( rule.dragWith != null ){
+    rule.dragWithMatches = getEleMatchesSpecFn( rule.dragWith );
+  }
+
+  rule.listener( function(){
+    update( cy, [ rule ] );
+  }, cy );
+
+  rule.enabled = true;
+
+  scratch.rules.push( rule );
+
+  return rule;
+};
+
+let bindForNodeList = function( cy, scratch ){
+  scratch.onAddNode = function( evt ){
+    let target = evt.target;
+
+    scratch.nodes.push( target );
+  };
+
+  cy.on('add', 'node', scratch.onAddNode);
+};
+
+let unbindForNodeList = function( cy, scratch ){
+  cy.removeListener('add', 'node', scratch.onAddNode);
+};
+
+let update = function( cy, rules ){
+  let scratch = cy.scratch().automove;
+
+  rules = rules != null ? rules : scratch.rules;
+
+  cy.batch(function(){ // batch for performance
+    for( let i = 0; i < rules.length; i++ ){
+      let rule = rules[i];
+
+      if( rule.destroyed || !rule.enabled ){ break; } // ignore destroyed rules b/c user may use custom when()
+
+      let nodes = rule.nodes || scratch.nodes;
+
+      for( let j = nodes.length - 1; j >= 0; j-- ){
+        let node = nodes[j];
+
+        if( node.removed() ){ // remove from list for perf
+          nodes.splice( j, 1 );
+          continue;
+        }
+
+        if( !rule.matches(node) ){ continue; }
+
+        let pos = node.position();
+        let newPos = rule.getNewPos( node );
+        let newPosIsDiff = newPos != null && ( pos.x !== newPos.x || pos.y !== newPos.y );
+
+        if( newPosIsDiff ){ // only update on diff for perf
+          node.position( newPos );
+
+          node.trigger( 'automove', [rule] );
+        }
+      }
+    }
+  });
+};
+
+let automove = function( options ){
+  let cy = this;
+
+  let scratch = cy.scratch().automove = cy.scratch().automove || {
+    rules: []
+  };
+
+  if( scratch.rules.length === 0 ){
+    scratch.nodes = cy.nodes().toArray();
+
+    bindForNodeList( cy, scratch );
+  }
+
+  if( options === 'destroy' ){
+    scratch.rules.forEach(function( r ){ r.destroy(); });
+    scratch.rules.splice( 0, scratch.rules.length );
+
+    unbindForNodeList( cy, scratch );
+
+    return;
+  }
+
+  let rule = addRule( cy, scratch, options );
+
+  update( cy, [ rule ] ); // do an initial update to make sure the start state is correct
+
+  return {
+    apply: function(){
+      update( cy, [ rule ] );
+    },
+
+    disable: function(){
+      this.toggle( false );
+    },
+
+    enable: function(){
+      this.toggle( true );
+    },
+
+    enabled: function(){
+      return rule.enabled;
+    },
+
+    toggle: function( on ){
+      rule.enabled = on !== undefined ? on : !rule.enabled;
+
+      if( rule.enabled ){
+        update( cy, [ rule ] );
+      }
+    },
+
+    destroy: function(){
+      let rules = scratch.rules;
+
+      unbindAllOnRule( rule );
+
+      rules.splice( rules.indexOf( rule ), 1 );
+
+      if( rules.length === 0 ){
+        unbindForNodeList( cy, scratch );
+      }
+
+      return this;
+    }
+  };
+};
+
+module.exports = automove;
