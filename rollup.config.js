eb26aff15b0c593dc54a9c9b576e1b8730bee3f9
Noaman Ahmed <noamanahmed@gmail.com>
2023-10-25 23:47:46 +0530

    added-config-minified

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
 const createEsm = process.env.CREATE_ESM === "true";
 const createCjs = process.env.CREATE_CJS === "true";
 const createUmd = process.env.CREATE_UMD === "true";
 const input = "src/index.js"; // Update with your entry point
-const outputDir = "build"; // Update with your desired output directory
-const name = "Cytposcape";
+const outputDir = "dist"; // Update with your desired output directory
+const name = "cytposcape-automove";
 const configs = [];
 
 if (createEsm) {
@@ -58,5 +59,37 @@ if (createUmd) {
     ],
   });
 }
+configs.push({
+  input,
+  output: {
+    file: `${outputDir}/cytoscape-automove.umd.min.js`,
+    format: "umd",
+    name: name,
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+    terser(),
+  ],
+});
+configs.push({
+  input,
+  output: {
+    file: `${outputDir}/cytoscape-automove.esm.min.js`,
+    format: "esm",
+    name: name,
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+    terser(),
+  ],
+});
 
 export default configs;
